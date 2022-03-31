323ca0ed5ad44bde5313ec38cbe5a8ddc651e161
shafiraaks <shafiraaks@gmail.com>
2022-03-31 23:49:40 +0700

    homework-8
    changing to hooks

--- homework-7/src/App.js
+++ homework-7/src/App.js
@@ -1,32 +1,28 @@
-import { Component } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Search from "./pages/search";
 
-class App extends Component {
-  state = {
-    access_token: "",
-    token_type: "",
-    expires_in: "",
-    state: "",
-    error: "",
-  };
+function App() {
+  const [access_token, setaccessToken] = useState("");
+  const [searchResult, setsearchResult] = useState([]);
+  const [searchQuery, setsearchQuery] = useState("");
 
-  handleAccess = () => {
+  const handleAccess = () => {
     window.location.href = `https://accounts.spotify.com/authorize?client_id=a62c7ba9e3b04ed593ed968fbeddcc1d&response_type=token&redirect_uri=http://localhost:3000&scope=playlist-modify-private`;
   };
 
-  handleSearch = async () => {
-    await fetch(`https://api.spotify.com/v1/search?q=${this.state.searchQuery.replaceAll(" ", "+")}&type=album&limit=12`, {
+  const handleSearch = useCallback(async () => {
+    await fetch(`https://api.spotify.com/v1/search?q=${searchQuery.replaceAll(" ", "+")}&type=album&limit=12`, {
       method: "GET",
       headers: {
-        Authorization: `Bearer ${this.state.access_token}`,
+        Authorization: `Bearer ${access_token}`,
       },
     })
       .then((res) => res.json())
-      .then((res) => this.setState({ searchResult: res.albums.items }));
-  };
+      .then((res) => setsearchResult(res.albums.items));
+  });
 
-  componentDidMount() {
+  useEffect(() => {
     const token =
       window.location.hash &&
       window.location.hash
@@ -35,27 +31,80 @@ class App extends Component {
         .find((elem) => elem.startsWith("access_token"))
         .replace("access_token=", "");
     if (token) {
-      this.setState({ access_token: token });
+      setaccessToken(token);
     }
-  }
+  });
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.access_token === "" ? <button onClick={this.handleAccess}>login</button> : <Search handleSearch={this.handleSearch} toggleFunction={(value) => this.setState} />}
-        {this.state.searchResult.map((item) => {
-          return (
-            <div key={item.id}>
-              <img src={item.images[1].url} />
-              <p>artist:{item.artist[1].name}</p>
-              <p>date:{item.release_date}</p>
-              <p>title={item.name}</p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {access_token === "" ? <button onClick={handleAccess}>login</button> : <Search handleSearch={handleSearch} toggleFunction={(value) => setsearchQuery(value)} />}
+      {searchResult.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.images[1].url} />
+            <p>{item.artists[0].name}</p>
+            <p>{item.release_date}</p>
+            <p>{item.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
+// class App extends Component {
+//   state = {
+//     access_token: "",
+//     searchResult: [],
+//     searchQuery: "",
+//   };
+
+//   handleAccess = () => {
+//     window.location.href = `https://accounts.spotify.com/authorize?client_id=a62c7ba9e3b04ed593ed968fbeddcc1d&response_type=token&redirect_uri=http://localhost:3000&scope=playlist-modify-private`;
+//   };
+
+//   handleSearch = async () => {
+//     await fetch(`https://api.spotify.com/v1/search?q=${this.state.searchQuery.replaceAll(" ", "+")}&type=album&limit=12`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${this.state.access_token}`,
+//       },
+//     })
+//       .then((res) => res.json())
+//       .then((res) => this.setState({ searchResult: res.albums.items }));
+//   };
+
+//   componentDidMount() {
+//     const token =
+//       window.location.hash &&
+//       window.location.hash
+//         .substring(1)
+//         .split("&")
+//         .find((elem) => elem.startsWith("access_token"))
+//         .replace("access_token=", "");
+//     if (token) {
+//       this.setState({ access_token: token });
+//     }
+//   }
+
+//   render() {
+//     console.log(this.state.searchResult);
+//     return (
+//       <div className="App">
+//         {this.state.access_token === "" ? <button onClick={this.handleAccess}>login</button> : <Search handleSearch={this.handleSearch} toggleFunction={(value) => this.setState({ searchQuery: value })} />}
+//         {this.state.searchResult.map((item) => {
+//           return (
+//             <div key={item.id}>
+//               <img src={item.images[1].url} />
+//               <p>{item.artists[0].name}</p>
+//               <p>{item.release_date}</p>
+//               <p>{item.name}</p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- homework-7/src/pages/search/index.js
+++ homework-7/src/pages/search/index.js
@@ -1,23 +1,45 @@
 import { Component } from "react";
 
-class Search extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggleFunction = props.toggleFunction;
-    this.handleSearch = props.handleSearch;
-  }
-
-  render() {
-    return (
-      <div>
-        <form>
-          <input value={this.state.searchQuery} placeholder="Search Here" onChange={(e) => this.toggleFunction(e.target.value)}></input>
-          <button onSubmit={(event) => this.handleSearch(event)}>Search</button>
-        </form>
-      </div>
-    );
-  }
+function Search({ toggleFunction, handleSearch }) {
+  return (
+    <div>
+      <form>
+        <input placeholder="Search Here" onChange={(e) => toggleFunction(e.target.value)}></input>
+        <button
+          onClick={() => {
+            handleSearch();
+          }}
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
+// class Search extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.toggleFunction = props.toggleFunction;
+//     this.handleSearch = props.handleSearch;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <form>
+//           <input placeholder="Search Here" onChange={(e) => this.toggleFunction(e.target.value)}></input>
+//           <button
+//             onClick={() => {
+//               this.handleSearch();
+//             }}
+//           >
+//             Search
+//           </button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 export default Search;
